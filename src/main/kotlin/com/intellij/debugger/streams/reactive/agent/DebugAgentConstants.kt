ad818dbb7214b34976e2f536c7bc53fc65d1c7a2
Aleksandr Eslikov <eslikov@gmail.com>
2021-05-11 08:48:57 +0300

    Add debug agent constants

---
+++ src/main/kotlin/com/intellij/debugger/streams/reactive/agent/DebugAgentConstants.kt
@@ -0,0 +1,13 @@
+// Copyright 2000-2021 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+package com.intellij.debugger.streams.reactive.agent
+
+/**
+ * @author Aleksandr Eslikov
+ */
+object DebugAgentConstants {
+    private const val PACKAGE = "com.intellij.debugger.streams.reacive.agent"
+
+    const val PACKAGE_INTERNAL = "com/intellij/debugger/streams/reactive/agent"
+    @Suppress("unused")
+    const val DEBUG_AGENT = "$PACKAGE.ReactiveDebuggerAgent"
+}
