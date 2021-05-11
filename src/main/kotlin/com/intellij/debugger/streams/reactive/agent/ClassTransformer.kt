be08a9d9d3c7f0eb97129551fcecd08815274d54
Aleksandr Eslikov <eslikov@gmail.com>
2021-05-11 08:48:26 +0300

    Add basic agent

---
+++ src/main/kotlin/com/intellij/debugger/streams/reactive/agent/ClassTransformer.kt
@@ -0,0 +1,42 @@
+// Copyright 2000-2021 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+package com.intellij.debugger.streams.reactive.agent
+
+import com.intellij.debugger.streams.reactive.agent.TransformerChecker.shouldTransform
+import net.bytebuddy.jar.asm.ClassReader
+import net.bytebuddy.jar.asm.ClassVisitor
+import net.bytebuddy.jar.asm.ClassWriter
+import java.lang.instrument.ClassFileTransformer
+import java.security.ProtectionDomain
+import java.util.concurrent.atomic.AtomicBoolean
+
+/**
+ * @author Aleksandr Eslikov
+ */
+internal open class ClassTransformer : ClassFileTransformer {
+    override fun transform(
+        loader: ClassLoader?,
+        className: String?,
+        clazz: Class<*>?,
+        protectionDomain: ProtectionDomain?,
+        classFile: ByteArray?
+    ): ByteArray? {
+
+        if (!shouldTransform(loader, className, clazz)) {
+            return null
+        }
+
+        val classReader = ClassReader(classFile)
+        val classWriter = ClassWriter(classReader, ClassWriter.COMPUTE_MAXS or ClassWriter.COMPUTE_FRAMES)
+
+        val changed = AtomicBoolean()
+        val classVisitor: ClassVisitor = ReactiveDebugClassVisitor(classWriter, changed)
+
+        classReader.accept(classVisitor, 0)
+
+        return if (!changed.get()) {
+            null
+        } else {
+            classWriter.toByteArray()
+        }
+    }
+}
